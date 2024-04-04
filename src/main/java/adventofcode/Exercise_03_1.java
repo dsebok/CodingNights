525bf1f679237af73e45ba08928fb40815f57bd7
Dave Sebok <dsebok@gmail.com>
2024-04-04 21:34:42 +0200

    [WIP] AdventOfCode exercise 3 part 1

--- src/main/java/adventofcode/Exercise_03_1.java
+++ src/main/java/adventofcode/Exercise_03_1.java
@@ -5,67 +5,83 @@ import io.CustomReader;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 public class Exercise_03_1 {
 
     public static void main(String[] args) {
         List<String> input = CustomReader.readFile("resources/input_03_1.txt");
-        // findPartNumbers --> List<Long>
-        // getSum
-        // syso
+        List<Long> partNumbers = findPartNumbers(input);
+        long sum = partNumbers.stream()
+                .mapToLong(a -> a)
+                .sum();
+        System.out.printf("The sum of the engine part numbers is: %d%n", sum);
     }
 
     private static List<Long> findPartNumbers(List<String> input) {
         List<Long> result = new ArrayList<>();
-        Optional<PositionedNumber> foundNumber = findFirstNumber(input);
-        while (foundNumber.isPresent()) {
-            PositionedNumber number = foundNumber.get();
+        Optional<PositionedNumber> optionalNumber = findNextNumber(input, 0 ,0);
+        while (optionalNumber.isPresent()) {
+            PositionedNumber number = optionalNumber.get();
             if (checkIfNumberContactsASymbol(input, number)) {
                 result.add(number.value);
             }
-            foundNumber = findNextNumber(input, number);
+            int searchStartIndex = number.endIndex + 1;
+            int currentLineIndex = number.lineIndex;
+            if (searchStartIndex >= input.get(currentLineIndex).length()) {
+                currentLineIndex++;
+                searchStartIndex = 0;
+            }
+            optionalNumber = findNextNumber(input, currentLineIndex, searchStartIndex);
         }
         return result;
     }
 
-    private static Optional<PositionedNumber> findFirstNumber(List<String> input) {
-        for (int i = 0; i < input.size(); i++) {
-            String line = input.get(i);
-            for (int j = 0; j < line.length(); j++) {
-                char ch = line.charAt(j);
+    /**
+     *
+     * @param input
+     * @param startLineIndex
+     * @param startIndex
+     * @return
+     */
+    private static Optional<PositionedNumber> findNextNumber(List<String> input, int startLineIndex, int startIndex) {
+        int currentStartIndex = startIndex;
+        for (int lineIndex = startLineIndex; lineIndex < input.size(); lineIndex++) {
+            String line = input.get(lineIndex);
+            for (int index = currentStartIndex; index < line.length(); index++) {
+                char ch = line.charAt(index);
                 if (Character.isDigit(ch)) {
-                    int row = i;
-                    int startIndex = j;
-                    StringBuilder sb = new StringBuilder(Character.toString(ch));
-                    int k = j;
-                    while (k < line.length()) {
-                        char endCh = line.charAt(k);
-                        if (Character.isDigit(endCh)) {
-                            sb.append(endCh);
-                        } else {
-                            break;
-                        }
-                        k++;
-                    }
-                    int endIndex = k - 1;
-                    PositionedNumber p = new PositionedNumber(row, startIndex, endIndex, Long.parseLong(sb.toString()));
-                    return Optional.of(p);
+                    PositionedNumber number = readNumber(line, lineIndex, index);
+                    return Optional.of(number);
                 }
             }
+            currentStartIndex = 0;
         }
         return Optional.empty();
     }
 
-    private static Optional<PositionedNumber> findNextNumber(List<String> input, PositionedNumber number) {
-        // TODO: implement
-        return Optional.empty();
+    /**
+     *
+     * @param line
+     * @param lineIndex
+     * @param startIndex
+     * @return
+     */
+    private static PositionedNumber readNumber(String line, int lineIndex, int startIndex) {
+        String linePart = line.substring(startIndex);
+        String textNumber = linePart.chars()
+                .takeWhile(Character::isDigit)
+                .mapToObj(codePoint -> String.valueOf((char) codePoint))
+                .collect(Collectors.joining());
+        int endIndex = startIndex + textNumber.length() - 1;
+        return new PositionedNumber(lineIndex, startIndex, endIndex, Long.parseLong(textNumber));
     }
 
     private static boolean checkIfNumberContactsASymbol(List<String> input, PositionedNumber number) {
         // TODO: implement
+        // symbol: if not digit AND not '.' char
         return false;
     }
 
-    private record PositionedNumber(int row, int startIndex, int endIndex, long value) {
-    }
+    private record PositionedNumber(int lineIndex, int startIndex, int endIndex, long value) {}
 }
