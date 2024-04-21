bdfa30987941baabd54077076586954665d9118c
Dave Sebok <dsebok@gmail.com>
2024-04-21 11:41:45 +0200

    AdventOfCode exercise 3 part 1

--- src/main/java/adventofcode/Exercise_03_1.java
+++ src/main/java/adventofcode/Exercise_03_1.java
@@ -6,11 +6,12 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class Exercise_03_1 {
 
     public static void main(String[] args) {
-        List<String> input = CustomReader.readFile("resources/input_03_1.txt");
+        List<String> input = CustomReader.readFile("resources/input_03.txt");
         List<Long> partNumbers = findPartNumbers(input);
         long sum = partNumbers.stream()
                 .mapToLong(a -> a)
@@ -78,9 +79,72 @@ public class Exercise_03_1 {
     }
 
     private static boolean checkIfNumberContactsASymbol(List<String> input, PositionedNumber number) {
-        // TODO: implement
-        // symbol: if not digit AND not '.' char
-        return false;
+        List<Character> contactChars = getContactChars(input, number);
+        return contactChars.stream()
+                .anyMatch(ch -> !Character.isDigit(ch) && ch != '.');
+    }
+
+    private static List<Character> getContactChars(List<String> input, PositionedNumber number) {
+        List<Character> result = new ArrayList<>();
+        result.addAll(getLeftSide(input, number.startIndex, number.lineIndex));
+        result.addAll(getRightSide(input, number.endIndex, number.lineIndex));
+        result.addAll(getTopSide(input, number.startIndex, number.endIndex, number.lineIndex));
+        result.addAll(getBottomSide(input, number.startIndex, number.endIndex, number.lineIndex));
+        return result;
+    }
+
+    private static List<Character> getLeftSide(List<String> input, int startIndex, int lineIndex) {
+        int colIndex = startIndex - 1;
+        if (colIndex < 0) {
+            return List.of();
+        }
+        List<Character> result = new ArrayList<>();
+        if (lineIndex > 0) {
+            result.add(input.get(lineIndex - 1).charAt(colIndex));
+        }
+        result.add(input.get(lineIndex).charAt(colIndex));
+        if (lineIndex + 1 < input.size()) {
+            result.add(input.get(lineIndex + 1).charAt(colIndex));
+        }
+        return result;
+    }
+
+    private static List<Character> getRightSide(List<String> input, int endIndex, int lineIndex) {
+        int colIndex = endIndex + 1;
+        if (colIndex >= input.get(lineIndex).length()) {
+            return List.of();
+        }
+        List<Character> result = new ArrayList<>();
+        if (lineIndex > 0) {
+            result.add(input.get(lineIndex - 1).charAt(colIndex));
+        }
+        result.add(input.get(lineIndex).charAt(colIndex));
+        if (lineIndex + 1 < input.size()) {
+            result.add(input.get(lineIndex + 1).charAt(colIndex));
+        }
+        return result;
+    }
+
+    private static List<Character> getTopSide(List<String> input, int startIndex, int endIndex, int lineIndex) {
+        int topLineIndex = lineIndex - 1;
+        if (topLineIndex < 0) {
+            return List.of();
+        }
+        String topLine = input.get(topLineIndex);
+        return IntStream.rangeClosed(startIndex, endIndex)
+                .mapToObj(topLine::charAt)
+                .toList();
+    }
+
+    private static List<Character> getBottomSide(List<String> input, int startIndex, int endIndex, int lineIndex) {
+        int bottomLineIndex = lineIndex + 1;
+        if (bottomLineIndex >= input.size()) {
+            return List.of();
+        }
+        String bottomLine = input.get(bottomLineIndex);
+        return IntStream.rangeClosed(startIndex, endIndex)
+                .mapToObj(bottomLine::charAt)
+                .toList();
     }
 
     private record PositionedNumber(int lineIndex, int startIndex, int endIndex, long value) {}
